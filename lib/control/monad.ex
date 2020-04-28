f64aafe59bd200cc17ba2d8961867c6d52d552e5
Guenther Schmidt <schmig68@gmail.com>
2020-04-28 02:21:39 -0500

    defined Monad, Applicative and Functor in terms of each other.

---
+++ lib/control/monad.ex
@@ -0,0 +1,28 @@
+defmodule Control.Monad do
+  @moduledoc false
+
+  defmacro __using__(_options) do
+    quote do
+      import unquote(__MODULE__)
+    end
+  end
+
+  defmacro inject_bind_return(bind, return) do
+    quote do
+      def ma ~>> a2mb do
+        unquote(bind).(ma, a2mb)
+      end
+
+      def return(value) do
+        unquote(return).(value)
+      end
+
+      require Control.Applicative
+
+      Control.Applicative.inject_pure_apply(
+        &return/1,
+        fn pa2b, pa -> pa2b ~>> fn a2b -> pa ~>> fn a -> pure(a2b.(a)) end end end
+      )
+    end
+  end
+end
