de64c2aa9854143371e1b797d564ba8c906aa489
Guenther Schmidt <schmig68@gmail.com>
2020-05-09 01:09:25 -0500

    reversed using >>> in macro

--- lib/control/do_notation.ex
+++ lib/control/do_notation.ex
@@ -41,7 +41,7 @@ defmodule Control.DoNotation do
       fn next ->
         fun.(
           quote do
-            unquote(line) >>> unquote(next)
+            unquote(line) ~>> fn _ -> unquote(next) end
           end
         )
       end,
