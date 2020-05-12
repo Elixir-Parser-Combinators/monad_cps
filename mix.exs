e291a2e8ea68d6f8d9c90da1c107d21a44e6e73b
Guenther Schmidt <schmig68@gmail.com>
2020-05-12 17:38:23 -0500

    adapted mix file, formatting

--- mix.exs
+++ mix.exs
@@ -7,22 +7,42 @@ defmodule Control.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/guenni68/monad_cps.git"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
+    []
+  end
+
+  defp description() do
+    """
+    This project provides the equivalent of a Monad Typeclass similar to Haskell.
+    The main difference though is that the typeclass is not abstract but the continuation monad.
+
+    Any other monad can be expressed using the continuation monad by first defining the *bind* and *return*
+    specific to the data type that constitutes the monad and then using 2 provided functions to *wrap* those
+    bind and return functions in their continuation monad counterparts.
+
+    Do Notation for convenience is provided via a macro defined in the Control.DoNotation module.
+
+    An example monad for the Maybe type in the README.
+    """
+  end
+
+  defp package() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/guenni68/monad_cps.git"}
     ]
   end
 end
