772f99691f775ac7978bc5f2f1d8d45173bd67c7
Guenther Schmidt <schmig68@gmail.com>
2020-05-12 16:53:49 -0500

    adding documentation

--- lib/control/continuation.ex
+++ lib/control/continuation.ex
@@ -1,6 +1,5 @@
 defmodule Control.Continuation do
   @moduledoc """
-
   This module provides the Continuation Monad, in which every other monad can be implemented.
 
   ```elixir
@@ -74,19 +73,20 @@ defmodule Control.Continuation do
     {@cont, fn fun -> fun.(a) end}
   end
 
-  # TODO
   @doc """
-
+  This is the classic monadic *bind* function. In Haskell this would be
+  ```
+  Monad a >>= \a -> Monad b
+  ```
   """
   def {@cont, inC} ~>> a2cb do
     cont(fn out -> inC.(fn a -> run_cont(a2cb.(a), out) end) end)
   end
 
-  # TODO
   @doc """
-
+  Shortcut for ~>>
   """
-    def ca >>> cb do
+  def ca >>> cb do
     ca ~>> fn _ -> cb end
   end
 
@@ -99,57 +99,63 @@ defmodule Control.Continuation do
     inC.(fun)
   end
 
-  # TODO
   @doc """
+  allows for early exits.
   """
-    def call_cc(fun) do
+  def call_cc(fun) do
     cont(fn out -> run_cont(fun.(fn a -> cont(const(out.(a))) end), out) end)
   end
 
   # Applicative implementation
-  # TODO
   @doc """
-
+  Any Monad instance is also an instance of Applicative. This function is equivalent to *return*
   """
   def pure(a) do
     return(a)
   end
 
+  @doc """
+  applies a function from a -> b that is within the Monad to a Monad of type a and produces a Monad of type b.
+  """
   def apa2b <~> apa do
     apa2b ~>> fn a2b -> apa ~>> fn a -> pure(a2b.(a)) end end
   end
 
   # Functor implementation
-  # TODO
   @doc """
-
+  Applies a function of type a -> b to a Functor of type a. Produces a Functor of type b.
   """
-    def fmap(a2b, fa) do
+  def fmap(a2b, fa) do
     pure(a2b) <~> fa
   end
 
   # helper functions
+  @doc """
+  Take a value x and produces a function that ignores its input and produces x.
+  """
   def const(x) do
     fn _ -> x end
   end
 
+  @doc """
+  Identity function
+  """
   def id(x) do
     x
   end
 
   # injections
-  # TODO
   @doc """
-
+  This functions produces a "lift" function, that lifts the monad instances into the continuation monad.
   """
-    def make_ic(bind) do
+  def make_ic(bind) do
     fn m -> cont(fn fred -> bind.(m, fred) end) end
   end
 
   @doc """
-
+  Produces a function that "executes" the continuation monad.
   """
-    def make_run(return) do
+  def make_run(return) do
     fn m -> run_cont(m, return) end
   end
 end
